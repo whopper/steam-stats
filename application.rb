b2013ff786b76de577d85cec8cdbe994273dd32c
Will Hopper <whopper@puppetlabs.com>
2015-10-03 15:00:26 -0700

    Update logic for lifespan percentage

--- application.rb
+++ application.rb
@@ -12,16 +12,23 @@ post '/playtime' do
 end
 
 def calculate_playtime(playerid)
-  data = JSON.load(open("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=AADA11508386B68F3D1E54F972C57845&steamid=#{playerid}&format=json"))
+  begin
+    data = JSON.load(open("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=AADA11508386B68F3D1E54F972C57845&steamid=#{playerid}&format=json"))
+  rescue
+    data = nil
+  end
 
   total = 0
-  data['response']['games'].each do |game|
-    total = total + game['playtime_forever']
+  if data
+    data['response']['games'].each do |game|
+      total = total + game['playtime_forever']
+    end
   end
 
   @minutes = total
-  @hours   = (total / 60.0).round(2)
-  @days    = (total / 60.0 / 24.0).round(2)
+  @hours   = total / 60
+  @days    = total / 60 / 24
   @months  = (total / 60.0 / 24.0 / 30.0).round(2)
   @years   = (total / 60.0 / 24.0 / 30.0 / 12.0).round(2)
+  @lifespan_percent = (@years / 78.0).round(3)
 end
